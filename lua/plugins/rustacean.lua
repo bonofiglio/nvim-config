ce4382b999b9130424d078ea25632a5a5bded1c1
Daniel Bonofiglio <dev@dan.uy>
2026-01-17 21:37:27 -0300

    fix: prevent rust-analyzer from scanning .direnv directory

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -2,10 +2,22 @@ vim.g.rustaceanvim = {
   tools = {
     test_executor = "background",
   },
+  server = {
+    default_settings = {
+      ["rust-analyzer"] = {
+        files = {
+          excludeDirs = {
+            -- Avoid incredibly long scans when using direnv
+            ".direnv",
+          },
+        },
+      },
+    },
+  },
 }
 
 return {
   "mrcjkb/rustaceanvim",
   version = "^5", -- Recommended
-  lazy = false, -- This plugin is already lazy
+  lazy = false,   -- This plugin is already lazy
 }
